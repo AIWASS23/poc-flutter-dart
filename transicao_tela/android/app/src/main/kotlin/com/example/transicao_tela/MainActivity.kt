7d1b4b51f723387b86d6991dbee8f2358abc0d8f
AIWASS23 <marcelodearaujo677@gmail.com>
2023-05-17 08:32:19 -0300

    transicao de telas

---
+++ transicao_tela/android/app/src/main/kotlin/com/example/transicao_tela/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.transicao_tela
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ transicao_tela/lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+import 'home_page.dart';
+import 'user_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      initialRoute: '/',
+      routes: {
+        '/':(context) => const HomePage(title: 'Mandala'),
+        '/user-page': (context) => const UserPage(),
+      },
+    );
+  }
+}
